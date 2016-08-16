718dae42ca4c9f96cf762483525a4e93b03c7a87
lmesz <lacienator@gmail.com>
2016-08-16 21:38:16 +0200

    comments: remove comments
     * use logger instead of puts
     * use filter in case of check of key existence

--- lib/aws-handler.rb
+++ lib/aws-handler.rb
@@ -1,7 +1,8 @@
-require 'aws-sdk'
+require 'logger'
 require 'net/ssh'
 require 'net/scp'
 require 'net/http'
+require 'aws-sdk'
 
 class AwsHandler
   REGION = 'us-east-1'.freeze
@@ -10,21 +11,16 @@ class AwsHandler
 
   def initialize()
     @ec2 = Aws::EC2::Resource.new(region: REGION)
+    @logger = Logger.new(STDOUT)
   end
 
   def create_key_if_not_exists()
-
-    puts 'Check if key exists ...'
-    @ec2.key_pairs().each do |key|
-      if key.name == KEY_NAME
-        puts "#{KEY_NAME} exists"
-        raise "#{KEY_NAME} file doesn't exists local !" unless is_key_downloaded
-        return
-      end
+    @logger.info('Check if key exists ...')
+    if @ec2.key_pairs(filters: [{name: 'key-name', values: [KEY_NAME] }]).first
+      @logger.info('Key exists check if it is downloaded ...')
+      create_key unless is_key_downloaded
     end
-
-    puts 'Create key ...'
-    create_key()
+    create_key
   end
 
   def is_key_downloaded()
@@ -42,17 +38,16 @@ class AwsHandler
 
     vpc_name = 'TestVPC'
 
-    puts 'Create vpc ...'
+    @logger.info('Create vpc ...')
     vpc = @ec2.vpcs(filters: [{name: 'tag:Name', values: [vpc_name]}])
     if vpc.first.instance_of? Aws::EC2::Vpc
       return vpc.first.id
     end
 
-    puts 'VPC does not exists create it'
+    @logger.info('VPC does not exists create it')
 
     vpc = @ec2.create_vpc({cidr_block: '10.200.0.0/16'})
 
-    # So we get a public DNS
     vpc.modify_attribute({
                              enable_dns_support: {value: true}
                          })
@@ -61,7 +56,6 @@ class AwsHandler
                              enable_dns_hostnames: {value: true}
                          })
 
-    # Name our VPC
     vpc.create_tags({tags: [{key: 'Name', value: vpc_name}]})
     igw_id = attach_vpc_to_internet_gateway('vpc-0e670569') #vpc.vpc_id)
 
@@ -73,15 +67,15 @@ class AwsHandler
   def attach_vpc_to_internet_gateway(vpc_id)
     internet_gateway_name = 'TestIGW'
 
-    puts 'Check if Internet Gateway already exists ...'
+    @logger.info('Check if Internet Gateway already exists ...')
 
     igw = @ec2.internet_gateways(filters: [{name: 'tag:Name', values: [internet_gateway_name]}])
     if igw.first.instance_of? Aws::EC2::InternetGateway
-      puts 'Internet gateway exists ...'
+      @logger.info('Internet gateway exists ...')
       return igw.first.id
     end
 
-    puts 'Internet Gateway does not exists, create it ...'
+    @logger.info('Internet Gateway does not exists, create it ...')
 
     igw = @ec2.create_internet_gateway
 
@@ -91,34 +85,34 @@ class AwsHandler
   end
 
   def create_route_table_to_internet_gateway(vpc_id, igw_id)
-    puts 'Add the internet gateway to the route tables ...'
+    @logger.info('Add the internet gateway to the route tables ...')
 
     route_table = @ec2.route_tables(filters: [{name: 'vpc-id', values: [vpc_id]}])
 
     if route_table.first.instance_of? Aws::EC2::RouteTable
-      puts 'Route found for the vpc ...'
+      @logger.info('Route found for the vpc ...')
       route_table.first.create_route({
                                          destination_cidr_block: '0.0.0.0/0',
                                          gateway_id: igw_id
                                      })
       return
     end
-    puts "Route table not found!"
+    @logger.info("Route table not found!")
   end
 
 
   def create_subnet_if_not_exists(vpc_id)
     subnet_name = 'TestSubnet'
 
-    puts 'Check if subnet exists ...'
+    @logger.info('Check if subnet exists ...')
 
     subnet = @ec2.subnets(filters: [{name: 'tag:Name', values: [subnet_name]}])
     if subnet.first.instance_of? Aws::EC2::Subnet
-      puts 'Subnet exists'
+      @logger.info('Subnet exists')
       return subnet.first.id
     end
 
-    puts 'Subnet does not exists create it'
+    @logger.info('Subnet does not exists create it')
 
     subnet = @ec2.create_subnet({
                                     vpc_id: vpc_id,
@@ -127,7 +121,6 @@ class AwsHandler
                                 })
 
     subnet.create_tags({tags: [{key: 'Name', value: subnet_name}]})
-    puts subnet.id
     return subnet.id
   end
 
@@ -135,16 +128,16 @@ class AwsHandler
 
     security_group_name = 'TestSecurityGroup'
 
-    puts 'Check if security group exists ...'
+    @logger.info('Check if security group exists ...')
 
     sec_group = @ec2.security_groups(filters: [{name: 'group-name', values: [security_group_name]}])
 
     if sec_group.first.instance_of? Aws::EC2::SecurityGroup
-      puts 'Security group exists'
+      @logger.info('Security group exists')
       return sec_group.first.id
     end
 
-    puts 'Security group does not exists, create...'
+    @logger.info('Security group does not exists, create...')
 
     sg = @ec2.create_security_group({
                                         group_name: security_group_name,
@@ -193,21 +186,19 @@ class AwsHandler
                                  }
                              ]
                          })
-
-    puts sg.id
     return sg.id
   end
 
   def create_instance(instance_name, sg_id, subnet_id)
-    puts 'Check if instance exists'
+    @logger.info('Check if instance exists')
 
     instance = @ec2.instances(filters: [{name: 'tag:Name', values: [instance_name]}])
     if instance.first.instance_of? Aws::EC2::Instance
-      puts "Instance already exists. Public DNS adress is #{instance.first.public_dns_name}"
+      @logger.info("Instance already exists. Public DNS adress is #{instance.first.public_dns_name}")
       return
     end
 
-    puts 'Instance does not exists, Create instance ...'
+    @logger.info('Instance does not exists, Create instance ...')
 
     user_data = File.read(File.join(File.dirname(__FILE__), '..', 'conf', 'user.data'))
 
@@ -226,32 +217,30 @@ class AwsHandler
                                                               }]
                                      })
 
-    # Wait for the instance to be created, running, and passed status checks
     @ec2.client.wait_until(:instance_status_ok, {instance_ids: [instance[0].id]})
 
-    # Name the instance 'TestInstance' and give it the Group tag 'TestGroup'
     instance.batch_create_tags({tags: [{key: 'Name', value: instance_name}, {key: 'Group', value: 'TestGroup'}]})
 
     inst = @ec2.instance(instance[0].id)
 
-    puts "The created instance public DNS address is: #{inst.public_dns_name}"
+    @logger.info("The created instance public DNS address is: #{inst.public_dns_name}")
   end
 
   def status(instance_name)
     instance = @ec2.instances(filters: [{name: 'tag:Name', values: [instance_name]}])
     if instance.first.instance_of? Aws::EC2::Instance
-      puts "Instance already exists. Public DNS adress is #{instance.first.public_dns_name}"
+      @logger.info("Instance already exists. Public DNS adress is #{instance.first.public_dns_name}")
       begin
         uri = URI("http://#{instance.first.public_dns_name}/")
         res = Net::HTTP.get_response(uri)
 
         if res.body.include? "drupal"
-          puts "Drupal is available at http://#{instance.first.public_dns_name}"
+          @logger.info("Drupal is available at http://#{instance.first.public_dns_name}")
         else
-          puts 'Drupal is not available, the host is listen on port 80, but does not serve drupal site!'
+          @logger.info('Drupal is not available, the host is listen on port 80, but does not serve drupal site!')
         end
       rescue Timeout::Error, SocketError
-        puts 'Drupal is not available, because nothing listen at port 80!'
+        @logger.error('Drupal is not available, because nothing listen at port 80!')
       end
     end
   end
