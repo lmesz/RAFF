3bbca4813975d2e87ac90ab831dc483cc6b707bf
lmesz <lacienator@gmail.com>
2016-09-17 19:34:55 +0200

    added failing testcase

--- tests/test_raff.rb
+++ tests/test_raff.rb
@@ -5,6 +5,6 @@ class TestRaff < Test::Unit::TestCase
   def test_raff()
     logger = Logger.new(STDOUT)
     aws_drupal_cluster_handler = AwsDrupalClusterHandler.new(logger)
-    assert_instance_of AwsDrupalClusterHandler, aws_drupal_cluster_handler
+    assert_instance_of AwsDrupalClusterHandler, adch
   end
 end
