9f0e2e18f748b36e96057422446437725df51211
lmesz <lacienator@gmail.com>
2016-09-18 17:00:29 +0200

    added first test for rest

--- tests/test_raff.rb
+++ tests/test_raff.rb
@@ -1,10 +1,20 @@
 require './lib/aws_drupal_cluster_handler'
+require './lib/aws_rest'
+
 require 'test/unit'
+require 'rack/test'
 
 class TestRaff < Test::Unit::TestCase
-  def test_raff()
+  def test_raff_cli()
     logger = Logger.new(STDOUT)
     adch = AwsDrupalClusterHandler.new(logger)
     assert_instance_of AwsDrupalClusterHandler, adch
   end
+
+  def test_raff_rest()
+    browser = Rack::Test::Session.new(Rack::MockSession.new(Sinatra::Application))
+    browser.get '/status', :instance_name => 'DummyInstanceName'
+    assert browser.last_response.ok?
+  end
+
 end
