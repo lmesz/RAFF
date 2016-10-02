81871942cef0c3d5d6e345f81d1fa14a90382f6f
lmesz <lacienator@gmail.com>
2016-10-02 17:13:51 +0200

    security group manager: added testcase for create... security group

--- spec/security_group_manager_spec.rb
+++ spec/security_group_manager_spec.rb
@@ -17,3 +17,37 @@ describe 'SecurityGroupManager Initialise' do
     end
   end
 end
+
+describe 'SecurityGroupManager create_security_group_if_not_exists' do
+  context 'when called and the security group exists' do
+    it 'returns the id' do
+      ec2mock = double('ec2')
+      allow(ec2mock).to receive(:security_groups).and_return([Aws::EC2::SecurityGroup.new(id: 42, stub_responses: true)])
+
+      loggermock = double('logger')
+      allow(loggermock).to receive(:info)
+
+      secgroupman = SecurityGroupManager.new(ec2mock, loggermock)
+      expect(secgroupman.create_security_group_if_not_exists('TestId')).to eq(42)
+    end
+  end
+
+  context 'when called and the security group does not exists' do
+    it 'creates it and return with the id' do
+      sgmock = double('security mock')
+      allow(sgmock).to receive(:authorize_egress)
+      allow(sgmock).to receive(:authorize_ingress)
+      allow(sgmock).to receive(:id).and_return(42)
+
+      ec2mock = double('ec2')
+      allow(ec2mock).to receive(:security_groups).and_return([])
+      allow(ec2mock).to receive(:create_security_group).and_return(sgmock)
+
+      loggermock = double('logger')
+      allow(loggermock).to receive(:info)
+
+      secgroupman = SecurityGroupManager.new(ec2mock, loggermock)
+      expect(secgroupman.create_security_group_if_not_exists('TestId')).to eq(42)
+    end
+  end
+end
