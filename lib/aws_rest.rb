c0610ae6db181db1e46b4e46196eea0884fb3fea
lmesz <lacienator@gmail.com>
2016-10-04 22:00:38 +0200

    aws_rest: simplified the routing and error handling remove a lot of redundancy

--- lib/aws_rest.rb
+++ lib/aws_rest.rb
@@ -6,52 +6,40 @@ require './lib/aws_drupal_cluster_handler'
 class AwsRest < Sinatra::Base
   set :port, 6666
 
-  def initialize(_app = nil, adch = AwsDrupalClusterHandler.new(Logger.new(STDOUT)))
+  def initialize(_app = nil, adch = AwsDrupalClusterHandler.new)
     super()
     @aws_drupal_cluster_handler = adch
   end
 
   post '/deploy/:instance_name' do
-    if !@aws_drupal_cluster_handler.deploy(params[:instance_name])
-      content_type :json
-      status 404
-      { result: 'error', message: 'Instance not found' }.to_json
-    else
-      content_type :json
-      { result: 'ok', message: 'Instance successfully deployed!' }.to_json
-    end
+    raise Sinatra::NotFound unless @aws_drupal_cluster_handler.deploy(params[:instance_name])
+    everything_ok_with_message('Instance successfully deployed!')
   end
 
   post '/stop/:instance_name' do
-    if !@aws_drupal_cluster_handler.stop(params[:instance_name])
-      content_type :json
-      status 404
-      { result: 'error', message: 'Instance not found' }.to_json
-    else
-      content_type :json
-      { result: 'ok', message: 'Instance successfully stoped!' }.to_json
-    end
+    raise Sinatra::NotFound unless @aws_drupal_cluster_handler.stop(params[:instance_name])
+    everything_ok_with_message('Instance successfully stoped!')
   end
 
   post '/terminate/:instance_name' do
-    if !@aws_drupal_cluster_handler.terminate(params[:instance_name])
-      content_type :json
-      status 404
-      { result: 'error', message: 'Instance not found' }.to_json
-    else
-      content_type :json
-      { result: 'ok', message: 'Instance successfully terminated!' }.to_json
-    end
+    raise Sinatra::NotFound unless @aws_drupal_cluster_handler.terminate(params[:instance_name])
+    everything_ok_with_message('Instance successfully terminated!')
   end
 
   get '/status/:instance_name' do
-    if !@aws_drupal_cluster_handler.status(params[:instance_name])
-      content_type :json
-      status 404
-      { result: 'error', message: 'Instance not found' }.to_json
-    else
-      content_type :json
-      { result: 'ok', message: 'Drupal is available on the given instance!' }.to_json
-    end
+    raise Sinatra::NotFound unless @aws_drupal_cluster_handler.status(params[:instance_name])
+    everything_ok_with_message('Drupal is available on the given instance!')
   end
+
+  def everything_ok_with_message(message)
+    content_type :json
+    { result: 'ok', message: message }.to_json
+  end
+
+  not_found do
+    content_type :json
+    status 404
+    { result: 'error', message: 'Instance not found' }.to_json
+  end
+
 end
