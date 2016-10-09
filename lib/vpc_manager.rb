aadc76b3f957956dfdd47067edeab87215c9bdec
lmesz <lacienator@gmail.com>
2016-10-09 16:12:09 +0200

    vpc manager: pattern followup

--- lib/vpc_manager.rb
+++ lib/vpc_manager.rb
@@ -3,19 +3,19 @@ require './lib/aws_base'
 
 class VpcManager < AwsBase
   def create_vpc_if_not_exists
-    vpc_tag_name = 'TestVPC'
+    begin
+      vpc_tag_name = 'TestVPC'
 
-    @logger.info('Create vpc ...')
-    vpc = @ec2.vpcs(filters: [{ name: 'tag:Name', values: [vpc_tag_name] }])
-    return vpc.first.id if vpc.first.instance_of? Aws::EC2::Vpc
+      @logger.info('Create vpc ...')
+      vpc = @ec2.create_vpc(cidr_block: '0.0.0.0/0')
 
-    @logger.info('VPC does not exists create it')
-    vpc = @ec2.create_vpc(cidr_block: '0.0.0.0/0')
+      configure_vpc(vpc, vpc_tag_name)
+      create_igw_for_vpc(vpc.vpc_id)
 
-    configure_vpc(vpc, vpc_tag_name)
-    create_igw_for_vpc(vpc.vpc_id)
-
-    vpc.vpc_id
+      vpc.vpc_id
+    rescue
+      raise VpcManagerException, 'Failed to create vpc.'
+    end
   end
 
   def configure_vpc(vpc, vpc_tag_name)
@@ -32,25 +32,13 @@ class VpcManager < AwsBase
 
   def attach_vpc_to_internet_gateway(vpc_id)
     igw_name = 'TestIGW'
-
-    @logger.info('Check if Internet Gateway already exists ...')
-
-    igw = @ec2.internet_gateways(filters: [{ name: 'tag:Name',
-                                             values: [igw_name] }])
-    if igw.first.instance_of? Aws::EC2::InternetGateway
-      @logger.info('Internet gateway exists ...')
-      return igw.first.id
-    end
-
     @logger.info('Internet Gateway does not exists, create it ...')
     create_igw(igw_name, vpc_id)
   end
 
   def create_igw(igw_name, vpc_id)
     igw = @ec2.create_internet_gateway
-
     sleep(10)
-
     igw.create_tags(tags: [{ key: 'Name',
                              value: igw_name }])
     igw.attach_to_vpc(vpc_id: vpc_id)
@@ -59,16 +47,10 @@ class VpcManager < AwsBase
 
   def create_route_table_to_internet_gateway(vpc_id, igw_id)
     @logger.info('Add the internet gateway to the route tables ...')
-    route_table = @ec2.route_tables(filters: [{ name: 'vpc-id',
-                                                values: [vpc_id] }])
-
-    if route_table.first.instance_of? Aws::EC2::RouteTable
-      @logger.info('Route found for the vpc ...')
-      route_table.first.create_route(destination_cidr_block: '0.0.0.0/0',
-                                     gateway_id: igw_id)
-      return
-    end
-
-    @logger.info('Route table not found!')
+    route_table.first.create_route(destination_cidr_block: '0.0.0.0/0',
+                                   gateway_id: igw_id)
   end
+end
+
+class VpcManagerException < RuntimeError
 end
--- spec/vpc_manager_spec.rb
+++ spec/vpc_manager_spec.rb
@@ -4,21 +4,20 @@ require_relative '../lib/vpc_manager'
 describe 'VpcManager Initialise' do
   context 'without parameters' do
     it 'all the parameters are default' do
-      vpcmanager = VpcManager.new
-
-      expect(vpcmanager.ec2).to be_a Aws::EC2::Resource
-      expect(vpcmanager.logger).to be_a Logger
+      vpc_manager = VpcManager.new
+      expect(vpc_manager.ec2).to be_a Aws::EC2::Resource
+      expect(vpc_manager.logger).to be_a Logger
     end
   end
 
   context 'with parameters' do
     it 'all the parameters used properly' do
-      ec2mock = double('ec2')
-      loggermock = double('logger')
-      vpcmanager = VpcManager.new(ec2mock, loggermock)
+      ec2_mock = double('ec2')
+      logger_mock = double('logger')
+      vpc_manager = VpcManager.new(ec2_mock, logger_mock)
 
-      expect(vpcmanager.ec2).to eq(ec2mock)
-      expect(vpcmanager.logger).to eq(loggermock)
+      expect(vpc_manager.ec2).to eq(ec2_mock)
+      expect(vpc_manager.logger).to eq(logger_mock)
     end
   end
 end
@@ -26,45 +25,36 @@ end
 describe 'VpcManager create_vpc_if_not_exists' do
   context 'when called and not found' do
     it 'creates one and return its id' do
-      vpcmock = double('vpc')
-      allow(vpcmock).to receive(:wait_until_exists)
-      allow(vpcmock).to receive(:modify_attribute)
-      allow(vpcmock).to receive(:create_tags)
-      allow(vpcmock).to receive(:vpc_id).and_return(42)
+      vpc_mock = double('vpc')
+      allow(vpc_mock).to receive(:wait_until_exists)
+      allow(vpc_mock).to receive(:modify_attribute)
+      allow(vpc_mock).to receive(:create_tags)
+      allow(vpc_mock).to receive(:vpc_id).and_return(42)
 
-      loggermock = double('logger')
-      allow(loggermock).to receive(:info)
+      logger_mock = double('logger')
+      allow(logger_mock).to receive(:info)
 
-      ec2mock = double('ec2')
-      allow(ec2mock).to receive(:vpcs).and_return(['asd'])
-      allow(ec2mock).to receive(:create_vpc).and_return(vpcmock)
+      ec2_mock = double('ec2')
+      allow(ec2_mock).to receive(:create_vpc).and_return(vpc_mock)
 
-      vpcmanager = VpcManager.new(ec2mock, loggermock)
-      allow(vpcmanager).to receive(:attach_vpc_to_internet_gateway)
-      allow(vpcmanager).to receive(:create_route_table_to_internet_gateway)
+      vpc_manager = VpcManager.new(ec2_mock, logger_mock)
+      allow(vpc_manager).to receive(:attach_vpc_to_internet_gateway)
+      allow(vpc_manager).to receive(:create_route_table_to_internet_gateway)
 
-      expect(ec2mock).to receive(:vpcs)
-      expect(vpcmanager.create_vpc_if_not_exists).to eq(42)
+      expect(vpc_manager.create_vpc_if_not_exists).to eq(42)
     end
   end
 
-  context 'when called and found' do
-    it 'returns the id' do
-      expectedvpcid = '42'
-      loggermock = double('logger')
-      allow(loggermock).to receive(:info)
+  context 'when error happens' do
+    it 'throws VpcManagerException' do
+      logger_mock = double('logger')
+      allow(logger_mock).to receive(:info)
 
-      aws_vpc = Aws::EC2::Vpc.new(id: '42',
-                                  region: 'us-east-1',
-                                  stub_responses: true)
+      ec2_mock = double('ec2')
+      allow(ec2_mock).to receive(:create_vpc).and_raise("just_raise")
 
-      allow(aws_vpc).to receive(:id).and_return(expectedvpcid)
-
-      ec2mock = double('ec2')
-      allow(ec2mock).to receive(:vpcs).and_return([aws_vpc])
-
-      vpcmanager = VpcManager.new(ec2mock, loggermock)
-      expect(vpcmanager.create_vpc_if_not_exists).to eq(expectedvpcid)
+      vpc_manager = VpcManager.new(ec2_mock, logger_mock)
+      expect { vpc_manager.create_vpc_if_not_exists }.to raise_error(VpcManagerException)
     end
   end
 end
