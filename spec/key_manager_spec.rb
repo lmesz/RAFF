bfa8a8e9876b79b0527903fc4011b7d67ee1d377
lmesz <lacienator@gmail.com>
2016-10-21 23:44:04 +0200

    fixed key manager testcase

--- spec/key_manager_spec.rb
+++ spec/key_manager_spec.rb
@@ -7,9 +7,11 @@ require_relative '../lib/key_manager'
 describe 'KeyManager ImportKey' do
   before :each do
     @ec2_mock = double('Ec2')
-    @logger_mock = double('Logger')
-    allow(@logger_mock).to receive(:info)
-    @keymanager = KeyManager.new(@ec2_mock, @logger_mock)
+    logger_mock = double('Logger')
+    allow(logger_mock).to receive(:info)
+    File.open('/tmp/TestKeyPub.key', 'w') {}
+    config='config.test'
+    @keymanager = KeyManager.new(@ec2_mock, logger_mock, config)
   end
 
   context 'when key exists' do
@@ -17,6 +19,7 @@ describe 'KeyManager ImportKey' do
       allow(@ec2_mock).to receive(:import_key_pair)
       expect(@ec2_mock).to receive(:import_key_pair)
       @keymanager.import_key_if_not_exists
+      expect(@keymanager.config['key']['key_path']).to eq('/tmp/')
     end
   end
 end
